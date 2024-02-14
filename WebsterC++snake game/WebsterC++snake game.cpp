d35cba75cd0b6f36f1af5c0d58821d13a5b74ff8
Abdurauf <154891685+AbduraufKZ@users.noreply.github.com>
2024-02-14 23:11:14 +0500

    so i drew the game board

--- WebsterC++snake game/WebsterC++snake game.cpp
+++ WebsterC++snake game/WebsterC++snake game.cpp
@@ -13,11 +13,46 @@ bool gameOver;
 void Setup() {
    
 }
-
+/*Student: Sherzod Aslonov;
+ This function displays the game on console, it draws game board, player and the current score of the player.
+  */
 void Draw() {
-   
-}
+    system("cls");
+    for (int i = 0; i < width + 2; i++) {
+        cout << "#";
+        cout << endl;
+    }
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "";
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "*";
+            else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o";
+                        print = true;
+                    }
+                }
+                if (!print)
+                    cout << " ";
+            }
 
+            if (j == width - 1)
+                cout << "";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "-";
+    cout << endl;
+    cout << "Score:" << score << endl;
+}
 /*Student: Muslima Rizayeva
 function that handles user input to control the game*/
 void Input() {
