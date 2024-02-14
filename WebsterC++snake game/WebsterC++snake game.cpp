db33b4cf808f7e5c8e824b6ea0adf7653fa7f20b
Abdurauf <154891685+AbduraufKZ@users.noreply.github.com>
2024-02-14 18:04:58 +0500

    so, i created file and here i did only main and savescore funtction and wrote void functions

--- WebsterC++snake game/WebsterC++snake game.cpp
+++ WebsterC++snake game/WebsterC++snake game.cpp
@@ -1,20 +1,54 @@
-// WebsterC++snake game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <fstream>//file handling save username
+#include <conio.h>
+#include <windows.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+bool gameOver;
+
+
+void Setup() {
+   
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void Draw() {
+   
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void Input() {
+   
+}
+
+void Logic() {
+    
+}
+//Abdurauf's part function for save the score
+void SaveScore(string name, int score) {
+    ofstream file;
+    file.open("snake_game_scores.txt", ios::out);
+    file << "Player: " << name << " Score: " << score << endl;
+    file.close();
+}
+
+int main() {
+    string playerName;
+    cout << "Enter your name: ";
+    cin >> playerName;
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        Sleep(10);
+    }
+    cout << "Game Over" << endl << "Your Score: " << score << endl;
+    SaveScore(playerName, score);
+    cout << "Press 'r' to restart or any other key to exit." << endl;
+    if (_getch() == 'r') {
+        main();
+    }
+    return 0;
+}
