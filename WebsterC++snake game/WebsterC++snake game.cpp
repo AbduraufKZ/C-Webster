3c0ec30307bc85620f8eb2d4980cff8e3fd54ca9
H2005M <aminov.mirzo.farxodovich@gmail.com>
2024-02-14 20:20:29 +0500

    This code controls the movement of the snake in the game "Snake". It tracks the snake's tail and updates its position to follow the head. The snake can move in four directions: left, right, up, and down. If the snake reaches the edge of the game field, it appears on the opposite side. Additionally, the code checks if the snake has collided with itself to prevent the game from ending. When the snake eats a fruit, its length increases, and the player earns points.

--- WebsterC++snake game/WebsterC++snake game.cpp
+++ WebsterC++snake game/WebsterC++snake game.cpp
@@ -21,9 +21,56 @@ void Draw() {
 void Input() {
    
 }
+/*Student: Aminov Humoyunmirzo
+function that controls the direction of movements */
+void Logic()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
 
-void Logic() {
-    
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+    if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
 }
 //Abdurauf's part function for save the score
 void SaveScore(string name, int score) {
